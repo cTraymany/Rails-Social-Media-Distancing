176d5eccb64252890dfd34aa91ee62389a1be79c
cTraymany <chindalatht@gmail.com>
2020-09-26 21:44:49 +0700

    Seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Clara', email: 'Clara@email.com', password: 'pw')
-Entry.create(title: 'Running is hard', content: 'Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! ', user_id: 1, goal_id: 1)
 Goal.create(name: 'Get fit', description: 'Exercise at least 30 minutes a day!')
+# goal_id: 1
+Goal.create(name: "Clean", description: "Mop the floors. Vacuum the carpet. Windex the windows. Do laundry.")
+# goal_id: 2
 
-Entry.create(title: 'House is So Messy!' content: "Wow, I haven't cleaned in weeks. I should probably do that! Wow, I haven't cleaned in weeks. I should probably do that! Wow, I haven't cleaned in weeks. I should probably do that! Wow, I haven't cleaned in weeks. I should probably do that! ", user_id: 1, goal_id: 1)
+User.create(name: 'Clara', email: 'Clara@email.com', password: 'pw')
+# user_id: 1
+Entry.create(title: 'Running is hard', content: 'Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! Today I ran 10 miles. Hooray! ', user_id: 1, goal_id: 1)
+Entry.create(title: 'House is So Messy!' content: "Wow, I haven't cleaned in weeks. I should probably do that! Wow, I haven't cleaned in weeks. I should probably do that! Wow, I haven't cleaned in weeks. I should probably do that! Wow, I haven't cleaned in weeks. I should probably do that! ", user_id: 1, goal_id: 2)
+
+User.create(name: 'Morticia', email: 'Morticia@email.com', password:'pw')
+# user_id: 2
+Entry.create(title: "New Year's Resolution!", content: "New year, new me! Let's get that bikini body! New year, new me! Let's get that bikini body! New year, new me! Let's get that bikini body! New year, new me! Let's get that bikini body! New year, new me! Let's get that bikini body!", user_id: 2, goal_id: 1)
+Entry.create(title: "Broke My New Year's Resolution", content: "Last night was a rager. Went ham on those canapés. Gotta clean the house now... Last night was a rager. Went ham on those canapés. Gotta clean the house now... Last night was a rager. Went ham on those canapés. Gotta clean the house now..." user_id: 2, goal_id: 2)
