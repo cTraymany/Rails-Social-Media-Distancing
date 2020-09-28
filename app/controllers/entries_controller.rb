d4a4467622dc046cc3d346d5a1bc89fd8c1afb65
cTraymany <chindalatht@gmail.com>
2020-09-28 11:48:52 +0700

    user/show user/entries user/entries/show paths are functional

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,20 @@
 class EntriesController < ApplicationController
     def index
-        @entries = current_user.entries
+        if params[:user_id].to_i == current_user.id
+            @user = current_user
+        else
+            redirect_to user_entries_path(current_user)
+        end
+
     end
     
     def show
-        # binding.pry
         @entry = current_user.entries.find_by(id: params[:id])
-        @goal = @entry.goal
-
-        
+        if @entry && params[:user_id].to_i == current_user.id
+            @goal = @entry.goal
+        else
+            redirect_to user_entries_path(current_user)
+        end
     end
 
 end
